7612548a5b5a36c673f5551b26f1a35e46dd1e77
Joe <pandajoey77@gmail.com>
2021-05-01 10:44:28 +0100

    pushing changed files

--- MultipassCluster.sh
+++ MultipassCluster.sh
@@ -40,6 +40,17 @@ multipass mount ~/nodejoinfiles master
 multipass mount ~/nodejoinfiles worker-1
 multipass mount ~/nodejoinfiles worker-2
 
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/CheckWorking.sh
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/MultipassCluster.sh
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/cleanup.sh
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/mastersetup.sh
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/vmcluster.yaml
+git clone https://github.com/PandaJoey/vm-cluster-test/blob/master/worker1setup.sh
+git cline https://github.com/PandaJoey/vm-cluster-test/blob/master/worker2setup.sh
+
+
+
+
 ## everything from here needs to be in yaml
 echo "###################"
 echo "entering master vm"
@@ -47,121 +58,6 @@ echo "###################"
 # enter shell for vm 1
 multipass shell master
 
-## ok we need to make files with this date in to copy to the vm to continue from here :()
-
-echo "####################"
-echo "installing microk8s"
-echo "####################"
-#install required files
-sudo snap install microk8s --classic --channel=1.18/stable
-microk8s status --wait-ready
-sudo usermod -a -G microk8s ubuntu
-sudo chown -f -R ubuntu ~/.kube
-
-echo "#########################"
-echo "creating node join file 1"
-echo "#########################"
-# add nodes to get info needs to somehow get the users name probably need to get inout
-sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > ~/nodejoinfiles/node1
-
-echo "#########################"
-echo "creating node join file 2"
-echo "#########################"
-
-sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > ~/nodejoinfiles/node2
-
-# save files from vm to pc
-exit
-
-echo "#########################"
-echo "entering vm 2"
-echo "#########################"
-# enter shell for vm 2
-multipass shell worker-1
-
-echo "####################"
-echo "installing microk8s"
-echo "####################"
-#install required files
-sudo snap install microk8s --classic --channel=1.18/stable
-microk8s status --wait-ready
-sudo usermod -a -G microk8s ubuntu
-sudo chown -f -R ubuntu ~/.kube
-
-echo "####################"
-echo "running join command"
-echo "####################"
-#get first join file to join cluster
-# somehow make it wait to join the cluster before exiting.
-sudo chmod +x /home/dev/nodejoinfiles/node1
-#could also exit and renter here to not have to sudo
-cd /home/dev/nodejoinfiles/
-
-sudo ./node1
-
-exit
-
-echo "####################"
-echo "entering vm 3"
-echo "####################"
-# enter shell for vm 3
-multipass shell worker-2
-
-echo "####################"
-echo "installing microk8s"
-echo "####################"
-#install required files
-sudo snap install microk8s --classic --channel=1.18/stable
-microk8s status --wait-ready
-sudo usermod -a -G microk8s ubuntu
-sudo chown -f -R ubuntu ~/.kube
-
-echo "####################"
-echo "running join command"
-echo "####################"
-#get first join file to join cluster
-# somehow make it wait to join the cluster before exiting.
-sudo chmod +x /home/dev/nodejoinfiles/node2
-cd /home/dev/nodejoinfiles/
-sudo ./node2
-
-exit
-
-echo "####################"
-echo "entering master vm"
-echo "####################"
-#renter vm1 to set up the cluster
-multipass shell master
-
-echo "####################"
-echo "installing app scaling and exposing ports"
-echo "####################"
-#deploy the cluster.
-microk8s kubectl create deployment microbot --image=dontrebootme/microbot:v1 #make sure this waits some time.
-microk8s kubectl scale deployment microbot --replicas=3
-microk8s kubectl expose deployment microbot --type=NodePort --port=80 --name=microbot-service
-
-
-
-echo "####################"
-echo "checking it worked"
-echo "####################"
-#check the cluster installed correctly 
-microk8s kubectl get all --all-namespaces
-
-echo "####################"
-echo "showing join ip"
-echo "####################"
-#get the ip address to connect too
-ip a | grep -Eo "[0-9][0-9]"".""[0-9][0-9][0-9]"".""[0-9][0-9][0-9]"".""[0-9][0-9][0-9]"| head -1 > /home/dev/nodejoinfiles/curlip
-exit
-
-echo "####################"
-echo "unmounting drives."
-echo "####################"
-multipass unmount ~/nodejoinfiles master
-multipass unmount ~/nodejoinfiles worker-1
-multipass unmount ~/nodejoinfiles worker-2
 
 #cd ~/nodejoinfiles/
 #curl curlip
