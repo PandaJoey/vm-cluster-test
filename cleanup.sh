cbe6297a46968b5135249d8ee8c8cdfd1a57e858
PandaJoey <up623013@myport.ac.uk>
2021-05-01 11:20:54 +0100

    Add files via upload

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+echo "####################"
+echo "unmounting drives."
+echo "####################"
+multipass unmount ~/nodejoinfiles master
+multipass unmount ~/nodejoinfiles worker-1
+multipass unmount ~/nodejoinfiles worker-2
+
+multipass stop master worker-1 worker-2
+multipass delete master worker-1 worker-2
+
+sudo snap remove multipass
---
+++ worker1setup.sh
@@ -0,0 +1,21 @@
+echo "####################"
+echo "installing microk8s"
+echo "####################"
+#install required files
+sudo snap install microk8s --classic --channel=1.18/stable
+microk8s status --wait-ready
+sudo usermod -a -G microk8s ubuntu
+sudo chown -f -R ubuntu ~/.kube
+
+echo "####################"
+echo "running join command"
+echo "####################"
+#get first join file to join cluster
+# somehow make it wait to join the cluster before exiting.
+sudo chmod +x /home/dev/nodejoinfiles/node1
+#could also exit and renter here to not have to sudo
+cd /home/dev/nodejoinfiles/
+
+sudo ./node1
+
+exit
