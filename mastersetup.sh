fb6363f3bbd0102edf67f21f78ec6bb9c78337b4
PandaJoey <up623013@myport.ac.uk>
2021-05-09 16:45:00 +0100

    changed directory for files again

--- mastersetup.sh
+++ mastersetup.sh
@@ -4,7 +4,8 @@ echo "####################"
 echo "installing microk8s"
 echo "####################"
 #install required files
-sudo apt update -y && sudo upgrade -y
+sudo apt update -y 
+sudo upgrade -y
 sudo snap install microk8s --classic --channel=1.18/stable
 microk8s status --wait-ready
 sudo usermod -a -G microk8s ubuntu
@@ -14,13 +15,13 @@ echo "#########################"
 echo "creating node join file 1"
 echo "#########################"
 # add nodes to get info needs to somehow get the users name probably need to get inout
-sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > /home/dev/dockerstuff/OwnScripts/vagrantcluster/node1
+sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > /home/node1
 
 echo "#########################"
 echo "creating node join file 2"
 echo "#########################"
 
-sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > /home/dev/dockerstuff/OwnScripts/vagrantcluster/node2
+sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > /home/node2
 
 # save files from vm to pc
 exit
