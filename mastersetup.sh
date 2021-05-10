9a7513bcc026c4c308a9d98c3e43cb1bc143565c
PandaJoey <up623013@myport.ac.uk>
2021-05-10 16:33:21 +0100

    trying to make machines see each other

--- mastersetup.sh
+++ mastersetup.sh
@@ -11,6 +11,25 @@ microk8s status --wait-ready
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu ~/.kube
 
+
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-bionic entry
+sed -e '/^.*ubuntu-focal64.*/d' -i /etc/hosts
+sed -i -e 's/#DNS=/DNS=8.8.8.8/' /etc/systemd/resolved.conf
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.168.33.13 master
+192.168.33.14 worker-1
+192.168.33.15 worker-2
+EOF
+
+apt-get update 
+apt-get install containerd -y
+
 echo "#########################"
 echo "creating node join file 1"
 echo "#########################"
