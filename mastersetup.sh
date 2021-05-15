6ccdde016ebfdb9a2ade43f61ca99d39d7bd8714
Joe <pandajoey77@gmail.com>
2021-05-15 12:44:45 +0100

    changed some stuff in master and worker scrits

--- mastersetup.sh
+++ mastersetup.sh
@@ -4,26 +4,12 @@ echo "####################"
 echo "installing microk8s"
 echo "####################"
 #install required files
-sudo apt update -y 
+sudo apt update -y
 sudo apt upgrade -y
 sudo snap install microk8s --classic --channel=1.18/stable
 sudo microk8s status --wait-ready
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu ~/.kube
-
-
-
-#echo "####################"
-#echo "getting address of the server."
-#echo "####################"
-#sudo IFNAME=$1
-#sudo ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
-#sudo sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
-
-# remove ubuntu-bionic entry
-#sudo sed -e '/^.*ubuntu-focal64.*/d' -i /etc/hosts
-#sudo sed -i -e 's/#DNS=/DNS=8.8.8.8/' /etc/systemd/resolved.conf
-
 
 echo "####################"
 echo "updating hosts"
@@ -35,11 +21,6 @@ sudo cat >> /etc/hosts <<EOF
 192.168.33.15 worker-2
 EOF
 
-#echo "####################"
-#echo "restarting the resolved host."
-#echo "####################"
-#service systemd-resolved restart
-
 echo "#########################"
 echo "creating node join file 1"
 echo "#########################"
@@ -49,8 +30,18 @@ sudo microk8s add-node | grep "192." > /home/node1
 echo "#########################"
 echo "creating node join file 2"
 echo "#########################"
-
 #sudo microk8s add-node | grep  "microk8s join" | grep -v "node with"| head -1 > /home/node2
 sudo microk8s add-node | grep "192." > /home/node2
 # save files from vm to pc
+
+echo "##################################################"
+echo "setting up kubernetes cluster and management"
+echo "##################################################"
+source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+alias k=kubectl
+complete -F __start_kubectl k
+
+kubectl apply -f  https://raw.githubusercontent.com/PandaJoey/vm-cluster-test/master/vmcluster.yaml
+
 exit
--- worker1setup.sh
+++ worker1setup.sh
@@ -4,22 +4,24 @@ echo "####################"
 echo "installing microk8s"
 echo "####################"
 #install required files
-sudo apt update -y 
+sudo apt update -y
 sudo apt upgrade -y
 sudo snap install microk8s --classic --channel=1.18/stable
 sudo microk8s status --wait-ready
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu ~/.kube
 
+echo "##################################################"
+echo "setting up kubernetes cluster and management"
+echo "##################################################"
+source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+alias k=kubectl
+complete -F __start_kubectl k
 
-#IFNAME=$1
-#ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
-#sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
-
-# remove ubuntu-bionic entry
-#sed -e '/^.*ubuntu-focal64.*/d' -i /etc/hosts
-#sed -i -e 's/#DNS=/DNS=8.8.8.8/' /etc/systemd/resolved.conf
-
+echo "#######################"
+echo "editing hosts files"
+echo "#######################"
 # Update /etc/hosts about other hosts
 sudo cat >> /etc/hosts <<EOF
 192.168.33.13 master
@@ -27,16 +29,17 @@ sudo cat >> /etc/hosts <<EOF
 192.168.33.15 worker-2
 EOF
 
-
 echo "####################"
 echo "running join command"
 echo "####################"
-#get first join file to join cluster
-# somehow make it wait to join the cluster before exiting.
-sudo chmod +x /vagrant/node1
-#could also exit and renter here to not have to sudo
-cd /vagrant/
-
-sudo ./node1
+if [ $HOSTNAME == "worker-1" ]
+then
+    sudo chmod +x /vagrant/node1
+    cd /vagrant/
+    sudo ./node1
+else
+    sudo chmod +x /vagrant/node2
+    cd /vagrant/
+    sudo ./node2
 
 exit
